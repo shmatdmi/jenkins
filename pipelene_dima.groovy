570d7068178f68457f3379af6d72c25cd2d25bc8
shmatdmi <85951653+shmatdmi@users.noreply.github.com>
2024-05-24 07:44:55 +0300

    Update pipelene_dima.groovy

--- pipelene_dima.groovy
+++ pipelene_dima.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                println ${JOB_NAME}
+                println "${JOB_NAME}"
                 echo "Another method is to use \${BUILD_NUMBER}, which is ${BUILD_NUMBER}"
             }
         }
