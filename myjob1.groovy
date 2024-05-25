933214345bf01b1bfda2ebbcc1a551257f8800f1
ngt@live.ru <ngt@live.ru>
2024-05-25 20:44:48 +0300

    edit myjob1 pipeline

--- myjob1.groovy
+++ myjob1.groovy
@@ -18,6 +18,7 @@ pipeline {
         }
         stage("Build") {
             steps {
+                echo "\033[32m==========================Build stage==========================\033[0m"
                 echo "Build stage."
                 echo "Hello $params.version"
             }
@@ -34,5 +35,58 @@ pipeline {
                 echo "Starting release on $params.env"
             }
         }
+    }
+    post {
+
+        // success {
+
+        //     mail to: 'dvshmatov@sberbank.ru', cc: 'dvshmatov@omega.sbrf.ru', subject: '[SUCCESS] Оповещение о результатах сканирования', body: "Тестовое сканирование ${env.REPO_URL}, ветка ${env.BRANCH} завершилось успешно \"Hello\"\nworld"
+
+        // }
+
+        failure {
+
+        mail to: 'ngt@live.ru', cc: 'shmatov787@gmail.ru', subject: '[CRITICAL] Оповещение о результатах сканирования', body: "Тестовое сканирование ${env.REPO_URL}, ветка ${env.BRANCH} завершилось неуспешно на этапе \"${env.FAILED_STAGE}\"\n${env.MESSAGE ?: ''}"
+
+            }
+
+        success {
+
+            emailext to: 'ngt@live.ru',
+
+                attachLog: true,
+
+                attachmentsPattern: '*.txt',
+
+                mimeType: 'text/html',
+
+                recipientProviders: [requestor()],
+
+                subject: "${JOB_BASE_NAME} - build:${BUILD_NUMBER} - successful",
+
+                body: """
+
+                    <div style= 'font-family:"Arial",sans-serif; background-color: #EEEEEE; border: 2px solid green;'>
+
+                    <h3 style='background-color:green; color:white; font-size: 19px;'>Успешное выполнение джобы ${JOB_BASE_NAME}</h3>
+
+                    <br>
+
+                    <p>Название репозитория:${env.REPO_URL}</p>
+
+                    <p>Лог сборки Jenkins во вложении</p>
+
+                    <br>
+
+                    </div>"""
+
+            }            
+
+        cleanup {
+
+            cleanWs disableDeferredWipeout: true, deleteDirs: true
+
+        }
+
     }
 }
