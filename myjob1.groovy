38131623d9456c09f3b236163c3a6f870c2e682e
ngt@live.ru <ngt@live.ru>
2024-05-25 20:20:08 +0300

    add pipeline config myjob1

---
+++ myjob1.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+    parameters {
+        booleanParam(name: "dryrun", defaultValue: true, description: "Тестовый запуск")
+        string(name: "version", defaultValue: "r48", trim: true, description: "Введите версию компонента")
+        text(name: "releaseNotes", defaultValue: "Добавлены новые feature", description: "Описание изменений в релизе")
+        password(name: "password", defaultValue: "changeme", description: "Введите пароль")
+        choice(name: "env", choices: ["PROD", "DEV", "UAT"], description: "Sample multi-choice parameter")
+    }
+    stages {
+        stage('DryRun') {
+            when {
+                expression { params.dryrun }
+            }
+            steps {
+                echo "THIS IS DRYRUN!"
+            }
+        }
+        stage("Build") {
+            steps {
+                echo "Build stage."
+                echo "Hello $params.version"
+            }
+        }
+        stage("Test") {
+            steps {
+                echo "Test stage."
+            }
+        }
+        stage("Release") {
+            steps {
+                echo "Defined release notes $params.releaseNotes"
+                echo "Starting release on $params.env"
+            }
+        }
+    }
+}
