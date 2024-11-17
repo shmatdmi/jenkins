73b7ddac4373aad6061e6a317574b235250e7740
Vasua Pupkin <you@gmail.com>
2024-11-17 11:21:37 +0300

    add env

--- if.groovy
+++ if.groovy
@@ -1,19 +1,37 @@
 pipeline {
-     stages {
-        stage ('Main Stage') {
+    agent any
+    environment {
+      APPLICATION_NAME="myapp"
+      REPOSITORY_NAME="myrepo"
+      FILENAME="${APPLICATION_NAME}-app.yaml"
+      ALREADY_EXISTS="false"
+    }
+    stages {
+        stage("Clone Git Repository") {
             steps {
-                script {
-                    if (true) {
-                        stage ('Stage 1') {
-                            sh 'echo Stage 1'
-                        }
-                    }
-                    if (false) {
-                        stage ('Stage 2') {
-                            sh 'echo Stage 2'
-                        }
-                    }
-                }
+                git(
+                    url: "https://github.com/shmatdmi/jenkins.git",
+                    branch: "main",
+                    changelog: true,
+                    credentialsId: 'mycreds',
+                    poll: true
+                )
+            }
+        }
+        stage("Check if file already exists") {
+            steps {
+                sh '''
+                  cd ./apps
+                  if [ -f "${FILENAME}" ]; then
+                    echo "${FILENAME} exists"
+                    ALREADY_EXISTS="true"
+                  else
+                    echo "${FILENAME} does not exist"
+                    ALREADY_EXISTS="false"
+                  fi
+                  echo "ALREADY_EXISTS = ${ALREADY_EXISTS}"
+                  cd ..
+                '''
             }
         }
     }
