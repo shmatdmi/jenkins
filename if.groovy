051ba0259b593a4557849038c98d8ebcf3277a97
Vasua Pupkin <you@gmail.com>
2024-11-30 17:30:45 +0300

    chage params

--- if.groovy
+++ if.groovy
@@ -32,7 +32,7 @@ pipeline {
             }
             steps {
                 echo "\033[32m==========================if else stage==========================\033[0m"
-                //sh 'cd ./data'
+                sh "cd ./data"
                 //sh "rm ./*"
                 //sh "ls -la ./data"
                 sh "curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric' > ./data/${APPLICATION_NAME}-weather.json"
