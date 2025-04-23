ee61760b15817b7a7bed3ffc5c351d2415cce17f
dima <ngt@live.ru>
2025-04-23 07:36:11 +0300

    add new pipeline

---
+++ weather.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    environment {
+      APPLICATION_NAME="myapp"
+      REPOSITORY_NAME="myrepo"
+      FILENAME="${APPLICATION_NAME}-weather.json"
+      ALREADY_EXISTS="false"
+    }
+    triggers {
+        cron('H 12 */3 * *')
+    }
+    options {
+        timestamps()
+        ansiColor('xterm')
+    }
+    parameters {
+        booleanParam(name: 'new_commit', defaultValue: true, description: 'Создание нового коммита')
+        booleanParam(name: 'if', defaultValue: true, description: 'if else stage')
+        string(name: 'BRANCH_TO_SCAN', defaultValue: 'main', trim: true, description: 'Ветка для сканирования')
+        choice(name: 'env', choices: ['PROD', 'DEV', 'IFT'], description: 'Sample multi-choice parameter')
+    }
+
+    stages {
+        stage ('curl') {
+            when {
+                expression {
+                    return params.if
+                }
+            }
+            steps {
+                echo "\033[32m==========================curl==========================\033[0m"
+                sh "curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric' > ${APPLICATION_NAME}"
+                echo "${APPLICATION_NAME}"
+            }
+        }
+    }
+    post {
+        cleanup {
+                cleanWs disableDeferredWipeout: true, deleteDirs: true
+        }
+    }
+}
