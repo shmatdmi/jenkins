c855bbd70473ee8ccd6edf66cea3759a6c60de92
dima <ngt@live.ru>
2025-04-23 07:50:50 +0300

    small fix

--- weather.groovy
+++ weather.groovy
@@ -22,7 +22,7 @@ pipeline {
             steps {
                 echo "\033[32m==========================curl==========================\033[0m"
                 script {
-                def data =  """ sh "curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric'" """
+                def data = "sh 'curl -m 2 https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric'"
                 echo "${data}"
                 }
             }
