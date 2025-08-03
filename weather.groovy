a7636a96ecbfb363b3fdcb023daf807eac4df250
dima <ngt@live.ru>
2025-08-03 15:29:31 +0300

    edit weather pipeline

--- weather.groovy
+++ weather.groovy
@@ -18,11 +18,11 @@ pipeline {
                 echo "\033[32m==========================curl==========================\033[0m"
                 script {
                 def data = """
-                 "curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric'"
+                 curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric'
                  """
                 echo "${data}"
                 echo """
-                 "curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric'"
+                 curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric'
                  """
                 }
             }
