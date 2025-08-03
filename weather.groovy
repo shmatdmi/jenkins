ef9ab739f025393ce71bcda68ed1271fecb1cb0e
dima <ngt@live.ru>
2025-08-03 16:04:25 +0300

    edit weather pipeline

--- weather.groovy
+++ weather.groovy
@@ -24,9 +24,6 @@ pipeline {
                     echo "Response from server: ${response}"
                     echo "\033[31m Waiting... \033[0m"
                     sleep 3
-                    mkdir ./data
-                    sh "curl -m 2 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric' > ./${APPLICATION_NAME}-weather.json"
-                    sh "ls -la ./data"
                 }
             }
         }
