93cfe99dfd120613671e76bd3b04a4a5a939823f
dima <ngt@live.ru>
2025-08-03 16:53:40 +0300

    test commit

--- weather.groovy
+++ weather.groovy
@@ -27,7 +27,6 @@ pipeline {
                     sh "rm -rf ./data"
                     sh "mkdir ./data"
                     sh "cd ./data"
-                    sh "curl -v 'https://api.openweathermap.org/data/2.5/weather?q=Moscow,RU&appid=ba23e3e7888484e7a26b57b215d65200&units=metric' > ./data/${APPLICATION_NAME}-weather.json"
                     sh "ls -la ./data"
                     // Предполагаем, что json хранится в файле
                     def jsonContent = readFile(file: './data/msk-weather.json')
