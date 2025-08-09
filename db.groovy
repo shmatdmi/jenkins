2da970428450692b79f231e4abae0f4b62be65ef
shmatdmi <sberlinux@ya.ru>
2025-08-09 14:47:48 +0300

    add new pipeline db

--- db.groovy
+++ db.groovy
@@ -31,4 +31,30 @@ pipeline {
             }
         }
     }
+}\
+
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Database Update') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(
+                        credentialsId: 'postgres-user-pass',
+                        usernameVariable: 'DB_USER',
+                        passwordVariable: 'DB_PASS'
+                    )]) {
+                        sh """
+                            psql -h 88.151.117.221 -U \"\$DB_USER\" -d postgres_db -w \\
+                                  -v PGPASSWORD=\"\$DB_PASS\" <<EOF
+                              SELECT * FROM public.cars';
+                            EOF
+                        """
+                    }
+                }
+            }
+        }
+    }
 }
