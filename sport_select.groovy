8d39fa5011b5d18465d12d4b36ebc351f77ad659
shmatdmi <sberlinux@ya.ru>
2026-02-21 18:51:46 +0300

    add select pipeline

--- sport_select.groovy
+++ sport_select.groovy
@@ -22,16 +22,33 @@ pipeline {
         stage('Database Update') {
             steps {
                 script {
-                    withCredentials([usernamePassword(
-                        credentialsId: 'postgres-user-pass',
-                        usernameVariable: 'DB_USER',
-                        passwordVariable: 'DB_PASS'
-                    )]) {
-                        sh """
-                            PGPASSWORD=\"\$DB_PASS\" psql -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U \"\$DB_USER\" -d ${POSTGRES_DBNAME} -w <<EOF
-                            select SUM(count_sport) from public.stat_sport where type_sport = '${params.TYPE}' AND create_date >= NOW() - INTERVAL '7 days';
-                            EOF
-                        """
+                    // Получаем результат SQL-запроса и сохраняем в переменную env.SUM_RESULT
+                    withCredentials([usernamePassword(credentialsId: 'postgres-user-pass', usernameVariable: 'DB_USER', passwordVariable: 'DB_PASS')]) {
+                        sh '''
+                            export SUM_RESULT=$(
+                                PGPASSWORD="\$DB_PASS" psql -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -U "\$DB_USER" -d "${POSTGRES_DBNAME}" -w <<EOF
+                                    select SUM(count_sport) from public.stat_sport where type_sport = '${params.TYPE}' AND create_date >= NOW() - INTERVAL '7 days';
+                                EOF
+                            )
+                        '''
+                    }
+                    
+                    echo "Сумма count_sport за последнюю неделю: \$SUM_RESULT"
+                }
+            }
+        }
+        
+        stage('Следующий этап') {
+            steps {
+                script {
+                    // Используем ранее сохранённую переменную
+                    if ("${env.SUM_RESULT}".trim()) {
+                        echo "Полученная сумма из предыдущего шага: ${env.SUM_RESULT}"
+                        
+                        // Здесь можешь продолжить работу с полученной суммой,
+                        // например отправить уведомление или выполнить дополнительные шаги
+                    } else {
+                        error("Ошибка при выполнении SQL-запроса")
                     }
                 }
             }
