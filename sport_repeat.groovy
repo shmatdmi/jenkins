0b495de47bfaa5a3193e402eee4cbc51ed8092c5
shmatdmi <sberlinux@ya.ru>
2026-03-09 18:20:55 +0300

    add new pipe with repeat

--- sport_repeat.groovy
+++ sport_repeat.groovy
@@ -15,34 +15,43 @@ pipeline {
     }
 
     parameters {
-        choice(name: 'TYPE', choices: ['Отжимания', 'Подтягивания', 'Пресс','Приседания', 'Бег'], description: 'Выберите вид упражений')
+        // Параметры упражнений
+        choice(name: 'TYPE', choices: ['Отжимания', 'Подтягивания', 'Пресс','Приседания', 'Бег'], description: 'Выберите вид упражнения')
         string(name: 'COUNT', defaultValue: '39', trim: true, description: 'Количество повторений')
-        string(name: 'LOCATION', defaultValue: 'Очаково', trim: true, description: 'Место выполнения упраженения')
+        string(name: 'LOCATION', defaultValue: 'Очаково', trim: true, description: 'Место выполнения упражнения')
         string(name: 'TIME', defaultValue: '1', trim: true, description: 'Время выполнения')
+        string(name: 'REPEAT_TIMES', defaultValue: '2', trim: true, description: 'Количество повторений сборки')
     }
 
     stages {
         stage('Database Update') {
             steps {
                 script {
-                    withCredentials([usernamePassword(
-                        credentialsId: 'postgres-user-pass',
-                        usernameVariable: 'DB_USER',
-                        passwordVariable: 'DB_PASS'
-                    )]) {
-                        sh """
-                            PGPASSWORD=\"\$DB_PASS\" psql -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U \"\$DB_USER\" -d ${POSTGRES_DBNAME} -w <<EOF
-                            insert into public.stat_sport(type_sport, count_sport, location_sport, time_sport) values ('${params.TYPE}', ${params.COUNT}, '${params.LOCATION}', '${params.TIME}');
-                            EOF
-                        """
+                    def repeatTimes = params.REPEAT_TIMES.toInteger() ?: 1
+                    
+                    for (def i = 1; i <= repeatTimes; i++) {
+                        echo "Выполнение шага №${i}"
+                        
+                        withCredentials([usernamePassword(
+                            credentialsId: 'postgres-user-pass',
+                            usernameVariable: 'DB_USER',
+                            passwordVariable: 'DB_PASS'
+                        )]) {
+                            sh """
+                                PGPASSWORD="\$DB_PASS" psql -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U "\$DB_USER" -d ${POSTGRES_DBNAME} -w <<EOF
+                                insert into public.stat_sport(type_sport, count_sport, location_sport, time_sport) values ('${params.TYPE}', ${params.COUNT}, '${params.LOCATION}', '${params.TIME}');
+                                EOF
+                            """
+                        }
                     }
                 }
             }
         }
     }
+    
     post {
         cleanup {
-                cleanWs()
+            cleanWs()
         }
     }
 }
