e5546252d02a502afa8d60a1502bed20c08fca8d
shmatdmi <sberlinux@ya.ru>
2026-06-14 10:09:55 +0300

    delete post

--- sport.groovy
+++ sport.groovy
@@ -24,17 +24,6 @@ pipeline {
     }
 
     stages {
-        stage('Setup SSH') {
-            steps {
-                sh '''
-                    mkdir -p /root/.ssh
-                    ssh-keyscan -t ed25519 github.com >> /root/.ssh/known_hosts 2>&1
-                    ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts 2>&1
-                    chmod 700 /root/.ssh
-                    chmod 600 /root/.ssh/known_hosts
-                '''
-            }
-        }
         stage('Database Update') {
             steps {
                 script {
@@ -57,12 +46,6 @@ pipeline {
                     }
                 }
             }
-        }
-    }
-    
-    post {
-        cleanup {
-            cleanWs()
         }
     }
 }
